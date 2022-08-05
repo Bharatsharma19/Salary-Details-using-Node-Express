a1a8e374743c36bc313b1f95dff925f46a271568
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-08-05 19:17:09 +0530

    05 Aug 2022

--- routes/employee.js
+++ routes/employee.js
@@ -1,12 +1,31 @@
-var express = require("express");
-var router = express.Router();
+let express = require("express");
+let router = express.Router();
 
 router.get("/", function (req, res) {
   res.render("employee");
 });
 
 router.get("/salary", function (req, res) {
-  res.render("salary", { month: "August" });
+  let date = new Date();
+
+  let month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let onlyMonth = month[date.getMonth()];
+
+  res.render("salary", { month: onlyMonth });
 });
 
 module.exports = router;
